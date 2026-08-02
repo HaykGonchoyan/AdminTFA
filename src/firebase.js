de85201ef64b732072161da0bcacfda7c8637ee1
Hayk Gonchoyan <hayk.gonchoyan@gmail.com>
2026-08-02 08:26:02 +0400

    Made use for .env

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC8sBs_EVEZKedIcWdhF4JCKZJWcs60-Bo",
-  authDomain: "admin-tfa.firebaseapp.com",
-  projectId: "admin-tfa",
-  storageBucket: "admin-tfa.firebasestorage.app",
-  messagingSenderId: "867268746908",
-  appId: "1:867268746908:web:89a68fa09ab92a268c9759",
-  measurementId: "G-KBFW42WWMR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: "G-70FLPKC14T"
 };
 
 // Initialize Firebase
